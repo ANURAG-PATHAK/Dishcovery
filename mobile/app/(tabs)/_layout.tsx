bd9b00a9d0af7fb9711e33cf9052ce15d8dd4edd
ANURAG-PATHAK <pathakanurag605@gmail.com>
2025-07-18 04:19:48 +0530

    Add Clerk and Expo Secure Store dependencies to mobile package.json

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { useAuth } from '@clerk/clerk-expo'
+import { Stack, Redirect } from 'expo-router'
+
+const TabLayout = () => {
+    const { isSignedIn } = useAuth();
+    if (!isSignedIn) return <Redirect href={'/(auth)/sign-in'} />
+    return (
+        <Stack />
+    )
+}
+
+export default TabLayout
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,18 +1,13 @@
-import { Stack } from "expo-router";
 import "./global.css";
 import '../nativewind-config';
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView className="flex-1">
-      <StatusBar style="dark" />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </SafeAreaView>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
   )
 }
